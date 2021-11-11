5d30d22f548dd7225ef1bf05c52a188555c1c1e5
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:39:34 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int sum = 0;
+        int product = 0;
+
+        Calculate(10, 20, out sum, out product);
+        Console.WriteLine("Sum is: {0}, and Product is: {1}", sum, product);
+    }
+
+    static void Calculate(int FN, int SN, out int Sum, out int Product)
+    {
+        Sum = FN + SN;
+        Product = FN * SN;
+        }
+    }
+}
